9ab572458dd16d32aa788c2a990eb59f7bf3c969
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2024-02-27 23:50:50 +0800

    Reset transaction version.

--- runtime/develop/src/runtime.rs
+++ runtime/develop/src/runtime.rs
@@ -57,7 +57,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_version: 1_000_000,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 4,
+    transaction_version: 1,
     state_version: 1,
 };
 
