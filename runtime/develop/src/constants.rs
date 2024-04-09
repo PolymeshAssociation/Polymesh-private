54d72535f1d3beafce592e17ae7969e9ae445b08
Adam Dossa <adam.dossa@gmail.com>
2024-04-09 17:47:39 +0100

    Update to default to 2 second block times

--- runtime/develop/src/constants.rs
+++ runtime/develop/src/constants.rs
@@ -5,9 +5,9 @@ pub mod time {
     use polymesh_primitives::{BlockNumber, Moment};
 
     #[cfg(feature = "ci-runtime")]
-    pub const MILLISECS_PER_BLOCK: Moment = 2000;
+    pub const MILLISECS_PER_BLOCK: Moment = 2_000;
     #[cfg(not(feature = "ci-runtime"))]
-    pub const MILLISECS_PER_BLOCK: Moment = 6000;
+    pub const MILLISECS_PER_BLOCK: Moment = 2_000;
     pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
     pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 30 * MINUTES;
     pub const EPOCH_DURATION_IN_SLOTS: u64 = {
